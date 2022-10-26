d421391e99c420a3418a8fdaa0a5b320025bb45d
Zaddish <40848785+Zaddish@users.noreply.github.com>
2022-10-27 06:03:36 +1100

    Delete payload8.js

--- payload8.js
+++
@@ -1,8 +0,0 @@
-const Http = new XMLHttpRequest();
-const url='http://pingb.in/p/249d4015bcbb7f5a65edb36aa79a';
-Http.open("GET", url);
-Http.send();
-
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-}
