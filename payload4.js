8e10fc2aec4eb53dc266b4a5d449a0f25c605913
Zaddish <40848785+Zaddish@users.noreply.github.com>
2022-10-27 05:20:18 +1100

    Create payload4.js

---
+++ payload4.js
@@ -0,0 +1,6 @@
+function contact_mothership() {
+    var http = new XMLHttpRequest();
+    var url = "http://pingb.in/p/249d4015bcbb7f5a65edb36aa79a";
+    http.open("GET", url, true);
+}
+contact_mothership();
